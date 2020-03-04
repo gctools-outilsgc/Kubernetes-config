5eb81d3eb1c368ed2e09ce83d079ebc171785a4a
Phanoix <phanoix@gmail.com>
2020-03-04 15:38:06 -0500

    cleanup for gitops part 1

--- aks.sh
+++ aks.sh
@@ -5,22 +5,23 @@ NODE_SIZE="Standard_DS2_v2"
 
 # Create aks cluster
 az aks create --name ${CLUSTER_NAME} --resource-group ${GROUP_NAME} \
-    --location canadacentral --kubernetes-version ${K8S_VERISON} \
-    --enable-cluster-autoscale true --min-count 1 --max-count 10 \
-    --node-vm-size ${NODE_SIZE}  --node-osdisk-size 50
+  --location canadacentral --kubernetes-version ${K8S_VERISON} \
+  --enable-cluster-autoscale --min-count 1 --max-count 10 \
+  --node-vm-size ${NODE_SIZE}  --node-osdisk-size 120
 
 # Set up kubectl to access the cluster api
 az aks get-credentials --name ${CLUSTER_NAME} --resource-group ${GROUP_NAME} --admin  
     # --overwrite-existing helps if you have an old cluster with the same name
 
 # Set up linkerd
-# will require linkerd cli
+# Requires linkerd cli
 linkerd install | kubectl apply -f -
 
 # Set up an nginx ingress instance with an azure FQDN
 
 
 # Install Fluxcd, sync with gitops repo
+# Requires fluxcd cli
 GHUSER="phanoix"
 GITOPS_REPO="gctools-outilsgc/flux-gitops-repo"
 
